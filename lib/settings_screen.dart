5ed1f10a4b728582d4f09f1dc132acbda10cb69c
SgtPreys <sgt.preys@gmail.com>
2026-07-06 19:26:10 +0200

    Version 1.1.2

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -219,7 +219,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ListTile(
             leading: const Icon(Icons.info_outline),
             title: const Text("App Version"),
-            trailing: const Text("1.1.1"), // Read-only
+            trailing: const Text("1.1.2"), // Read-only
           ),
           ListTile(
             leading: const Icon(Icons.email),
